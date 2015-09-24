1941000e948cd6546e24dd7d0965a4811081397f
Thomas Hutchinson <thomas.hutchinson@williamhill.com>
2015-09-24 15:54:33 +0200

    more examples

--- functions.erl
+++ functions.erl
@@ -5,7 +5,37 @@
 increase(By) -> fun(Number) -> Number + By end.
 
 
+
 logger() -> logger("TestApp", fun os:timestamp/0).
 
 logger(ApplicationName, TimeFunc) -> 
 	fun(Message) -> io:format("Time:~p Application:~p Message:~p ~n", [TimeFunc(), ApplicationName, Message]) end.
+
+
+logger_process() -> spawn(fun() -> logger(logger()) end).
+
+logger(Logger) ->
+	receive
+		Message -> Logger(Message)
+	end.
+
+
+%% sequence comprised of functions
+seq(From, From) -> fun() -> From end;
+seq(To, From) -> fun() -> {To, seq(To+1, From)} end.
+
+%% obviously wouldn't be used in something real
+cache(Function) -> fun(Arg0) -> cache(Function, get(Arg0), Arg0) end.
+
+cache(Function, undefined, Arg0) -> 
+	Results = Function(Arg0),
+	put(Arg0, Results),
+	Results;
+cache(_, CachedValue, _) -> CachedValue.
+
+cache_sleep() -> cache(fun sleep/1).
+
+sleep(Millis) -> 
+		receive
+		after Millis -> Millis
+	end.	
